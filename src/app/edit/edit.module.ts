7f621ab992bd423980ae82414b6e0693a98e87d1
H.K <kamakurahideki@HKnoMacBook-Pro.local>
2020-04-26 11:38:33 +0900

    feat(edit):フォーム画面作成

--- src/app/edit/edit.module.ts
+++ src/app/edit/edit.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { EditRoutingModule } from './edit-routing.module';
 import { EditComponent } from './edit/edit.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [EditComponent],
   imports: [
     CommonModule,
-    EditRoutingModule
-  ]
+    EditRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
 })
-export class EditModule { }
+export class EditModule {}
