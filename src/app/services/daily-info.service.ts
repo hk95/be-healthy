a278db0adece33e89cb3c8ba6ff59dfcaecb5fe8
H.K <kamakurahideki@HKnoMacBook-Pro.local>
2020-04-27 16:05:46 +0900

    feat(top):databaseに登録した情報をtopに表示 #37

--- src/app/services/daily-info.service.ts
+++ src/app/services/daily-info.service.ts
@@ -3,6 +3,8 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from '../interfaces/user';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,16 +14,35 @@ export class DailyInfoService {
     private db: AngularFirestore,
     private snackBar: MatSnackBar,
     private router: Router
-    ) {}
+  ) {}
 
   editDailyInfo(dailyInfo: User) {
     const dailyId = this.db.createId();
-    return this.db.doc(`dailyInfos/${dailyId}`).set(dailyInfo)
-    .then(() => {
-      this.snackBar.open('登録しました', null, {
-        duration: 2000
+    return this.db
+      .doc(`dailyInfos/${dailyId}`)
+      .set(dailyInfo)
+      .then(() => {
+        this.snackBar.open('登録しました', null, {
+          duration: 2000,
+        });
+        this.router.navigateByUrl('/top');
       });
-      this.router.navigateByUrl('/top');
-    });
+  }
+
+  getDailyInfo(userId: string): Observable<User> {
+    return this.db
+      .collection<User>('dailyInfos', (ref) =>
+        ref.where('userId', '==', userId)
+      )
+      .valueChanges()
+      .pipe(
+        map((dailyinfos) => {
+          if (dailyinfos.length) {
+            return dailyinfos[0];
+          } else {
+            return null;
+          }
+        })
+      );
   }
 }
--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, } from '@angular/core';
 import { User } from 'src/app/interfaces/user';
+import { DailyInfoService } from 'src/app/services/daily-info.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-top',
@@ -7,8 +10,13 @@ import { User } from 'src/app/interfaces/user';
   styleUrls: ['./top.component.scss']
 })
 export class TopComponent implements OnInit {
-
-  constructor() { }
+  dailyInfo$: Observable<User> = this.dailyInfoService.getDailyInfo(
+    this.authService.uid
+  );
+  constructor(
+    private dailyInfoService: DailyInfoService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
