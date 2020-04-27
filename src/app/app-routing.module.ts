cfb144436c068afbfd0e75a4a59dba80a3e00acd
H.K <kamakurahideki@HKnoMacBook-Pro.local>
2020-04-27 13:38:39 +0900

    feat(auth):ログイン、ログアウト機能追加　#30

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'top',
-    loadChildren: () => import('./top/top.module').then(m => m.TopModule)
+    loadChildren: () => import('./top/top.module').then((m) => m.TopModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'edit',
-    loadChildren: () => import('./edit/edit.module').then(m => m.EditModule)
-  }
+    loadChildren: () => import('./edit/edit.module').then((m) => m.EditModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('./welcome/welcome.module').then((m) => m.WelcomeModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
