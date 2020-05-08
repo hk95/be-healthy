7fe61ced005c9f4469ca5894389bf1798d6e54ed
H.K <kamakurahideki@HKnoMacBook-Pro.local>
2020-05-08 17:08:45 +0900

    fix(top):fix　detailの表示に変更

--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/interfaces/user';
-import { DailyInfoService } from 'src/app/services/daily-info.service';
-import { AuthService } from 'src/app/services/auth.service';
-import { Observable } from 'rxjs';
-import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-top',
@@ -11,20 +7,8 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./top.component.scss'],
 })
 export class TopComponent implements OnInit {
-  constructor(
-    private dailyInfoService: DailyInfoService,
-    private authService: AuthService,
-    private datepipe: DatePipe
-  ) {}
-  today: string = this.getDate();
-  dailyInfos$: Observable<User[]> = this.dailyInfoService.getDailyInfos(
-    this.authService.uid
-  );
-  isToday$: Observable<User> = this.dailyInfoService.isToday(this.today);
-  getDate() {
-    const d = new Date();
-    return this.datepipe.transform(d, 'yy.MM.dd(E)');
-  }
+  constructor() {}
+
 
   ngOnInit(): void {}
 }
