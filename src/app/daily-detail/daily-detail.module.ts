e3737503173b4b12cc773b7b90b2ab3a94938b12
H.K <kamakurahideki@HKnoMacBook-Pro.local>
2020-05-09 20:02:57 +0900

    feat(dailyDetail):ページ遷移の変更（クエリ→動的） #55

--- src/app/daily-detail/daily-detail.module.ts
+++ src/app/daily-detail/daily-detail.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DailyDetailRoutingModule } from './daily-detail-routing.module';
 import { DailyDetailComponent } from './daily-detail.component';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [DailyDetailComponent],
-  imports: [
-    CommonModule,
-    DailyDetailRoutingModule
-  ]
+  imports: [CommonModule, DailyDetailRoutingModule, MatButtonModule],
 })
-export class DailyDetailModule { }
+export class DailyDetailModule {}
