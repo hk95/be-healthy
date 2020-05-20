8d061cc948825bd2a20bf602508294d21d842cdf
Hideki Kamakura <kakihk695@gmail.com>
2020-05-20 11:32:29 +0900

    feat(auth):googleログイン機能追加#3

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth, User } from 'firebase/app';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { switchMap } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +14,19 @@ export class AuthService {
   afUser$: Observable<User> = this.afAuth.user;
   uid: string;
 
+  user$: Observable<User> = this.afAuth.authState.pipe(
+    switchMap((afUser) => {
+      if (afUser) {
+        return this.db.doc<User>(`users/${afUser.uid}`).valueChanges();
+      } else {
+        return of(null);
+      }
+    })
+  );
+
   constructor(
     private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
     private router: Router,
     private snackBar: MatSnackBar
   ) {
@@ -23,7 +36,9 @@ export class AuthService {
   }
 
   login() {
-    this.afAuth.signInWithPopup(new auth.GithubAuthProvider()).then(() => {
+    const provider = new auth.GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: 'select_account' });
+    this.afAuth.signInWithPopup(provider).then(() => {
       this.snackBar.open('ログインしました', null, {
         duration: 2000,
       });
