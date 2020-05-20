17a521e1c62364feb41e347cbaae262495c49ea0
Hideki Kamakura <kakihk695@gmail.com>
2020-05-20 11:20:00 +0900

    feat(index.ts):googleログイン機能追加　#3

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  return db.doc(`users/${user.uid}`).set({
+    name: user.displayName,
+    avatarURL: user.photoURL,
+    email: user.email,
+    createdAt: new Date(),
+    userId: user.uid,
+  });
+});
