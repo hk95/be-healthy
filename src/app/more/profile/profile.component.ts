c99a8ce5bbb6eb9920a0fbc61a3223f238f8a7df
Hideki Kamakura <kakihk695@gmail.com>
2020-05-21 12:28:44 +0900

    feat(style,profile):プロフィール入力画面作成　#16

--- src/app/more/profile/profile.component.ts
+++ src/app/more/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OthreShellService } from 'src/app/services/othre-shell.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { BasicInfoService } from 'src/app/services/basic-info.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,44 @@ import { OthreShellService } from 'src/app/services/othre-shell.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  constructor(private othreShellService: OthreShellService) {
-    this.othreShellService.setTitle('プロフィール');
+  form = this.fb.group({
+    userName: [''],
+    gender: ['', [Validators.pattern(/male|female|other/)]],
+    height: [''],
+    goalWeight: [''],
+    goalFat: [''],
+    goalCal: [''],
+  });
+  constructor(
+    private otherShellService: OthreShellService,
+    private basicInfoService: BasicInfoService,
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.otherShellService.setTitle('プロフィール');
+    this.basicInfoService
+      .getBasicInfo(this.authService.uid)
+      .subscribe((basicInfo) => {
+        if (basicInfo) {
+          this.form.patchValue(basicInfo);
+        } else {
+          console.log('error');
+        }
+      });
   }
 
   ngOnInit(): void {}
+
+  submit() {
+    const formData = this.form.value;
+    this.basicInfoService.updateBasicInfo({
+      userName: formData.userName,
+      gender: formData.gender,
+      height: formData.height,
+      goalWeight: formData.goalWeight,
+      goalFat: formData.goalFat,
+      goalCal: formData.goalCal,
+      authorId: this.authService.uid,
+    });
+  }
 }
