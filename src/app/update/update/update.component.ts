f7d142d19bf36cb35015a7e021bdcc9dacee2f41
Hideki Kamakura <kakihk695@gmail.com>
2020-05-22 15:51:38 +0900

    feat(update):スタイル調整、フォーム表示変更　#27

--- src/app/update/update/update.component.ts
+++ src/app/update/update/update.component.ts
@@ -1,50 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { DailyInfoService } from 'src/app/services/daily-info.service';
 import { ActivatedRoute } from '@angular/router';
 
 import { switchMap, take } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { DailyInfo } from 'src/app/interfaces/daily-info';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.scss'],
 })
 export class UpdateComponent implements OnInit {
-  id: string;
+  @Input() dailyInfo: DailyInfo;
+  dailyId: string;
+  dailyInfo$: Observable<DailyInfo>;
 
   form = this.fb.group({
-    weight: ['', [Validators.required]],
-    fat: ['', [Validators.required]],
-    totalCal: ['', [Validators.required]],
+    currentWeight: ['', [Validators.required]],
+    currentFat: ['', [Validators.required]],
+    dailyMemo: [''],
   });
 
   constructor(
     private fb: FormBuilder,
     private dailyInfoService: DailyInfoService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private location: Location
   ) {
-    this.route.queryParamMap.subscribe((params) => {
-      this.id = params.get('id');
-
-      this.dailyInfoService.getDailyInfo(this.id).subscribe((user) => {
-        if (user) {
-          this.form.patchValue(user);
-          console.log('wataru');
-        } else {
-          console.log('error');
-        }
-      });
+    this.route.paramMap.subscribe((params) => {
+      this.dailyId = params.get('id');
+      this.dailyInfoService
+        .getDailyInfo(this.authService.uid, this.dailyId)
+        .subscribe((dailyInfo) => {
+          if (dailyInfo) {
+            this.form.patchValue(dailyInfo);
+            console.log('wataru');
+          } else {
+            console.log('error');
+          }
+        });
     });
+    // this.route.queryParamMap.subscribe((params) => {
+    //   const id = params.get('id');
+
+    //   this.dailyInfoService
+    //     .getDailyInfo(this.authService.uid, id)
+    //     .subscribe((user) => {
+    //       if (user) {
+    //         this.form.patchValue(user);
+    //         console.log('wataru');
+    //       } else {
+    //         console.log('error');
+    //       }
+    //     });
+    // });
   }
   ngOnInit() {}
 
   updateSubmit() {
     const formData = this.form.value;
-    this.dailyInfoService.upadateDailyInfo({
-      weight: formData.weight,
-      fat: formData.fat,
-      totalCal: formData.totalCal,
-      id: this.id,
+    this.dailyInfoService.updateDailyInfo({
+      dailyId: this.dailyId,
+      currentWeight: formData.currentWeight,
+      currentFat: formData.currentFat,
+      dailyMemo: formData.dailyMemo,
+      authorId: this.authService.uid,
     });
+  }
+  back() {
+    this.location.back();
   }
 }
