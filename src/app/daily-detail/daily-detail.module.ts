a1c6c7898517c6629946d8123701de78250bcaf0
Hideki Kamakura <kakihk695@gmail.com>
2020-05-22 15:50:33 +0900

    feat(daily-detail):スタイル調整　#27

--- src/app/daily-detail/daily-detail.module.ts
+++ src/app/daily-detail/daily-detail.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { DailyDetailRoutingModule } from './daily-detail-routing.module';
 import { DailyDetailComponent } from './daily-detail.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [DailyDetailComponent],
-  imports: [CommonModule, DailyDetailRoutingModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    DailyDetailRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class DailyDetailModule {}
