a92c979dacecffc2fbf8e793782229717001e21a
Hideki Kamakura <kakihk695@gmail.com>
2020-05-29 01:47:14 +0900

    fix(daily-info):インターフェイス修正  #32

--- src/app/interfaces/daily-info.ts
+++ src/app/interfaces/daily-info.ts
@@ -1,17 +1,25 @@
 import { User } from './user';
+import { DailyInfoService } from '../services/daily-info.service';
+import { OriginalFood } from './original-food';
 
 export interface DailyInfo {
   dailyId: string;
   date: string;
   currentWeight: number;
   currentFat: number;
-  breakfast: number;
+  breakfast: Breakfast;
   lunch: number;
-  denner: number;
+  dinner: number;
   dailyMemo: string;
   authorId: string;
 }
 
-interface UserWithAuthor extends User {
-  author: User;
+export interface Breakfast {
+  breakfastId: string;
+  foodId: string;
+  amount: number;
+}
+
+export interface BreakfastWithMeal extends Breakfast {
+  meal: OriginalFood;
 }
