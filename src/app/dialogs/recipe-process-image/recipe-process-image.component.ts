3124b84670ecd652737786ef6c8d7609178ff233
Hideki Kamakura <kakihk695@gmail.com>
2020-06-12 11:59:06 +0900

    feat(recipe-process-component):ダイアログ実装　#80

---
+++ src/app/dialogs/recipe-process-image/recipe-process-image.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from 'src/app/services/auth.service';
+import { ImageCroppedEvent, base64ToFile } from 'ngx-image-cropper';
+import { RecipeService } from 'src/app/services/recipe.service';
+
+@Component({
+  selector: 'app-recipe-process-image',
+  templateUrl: './recipe-process-image.component.html',
+  styleUrls: ['./recipe-process-image.component.scss'],
+})
+export class RecipeProcessImageComponent implements OnInit {
+  croppedImage: string;
+  userId: string = this.authSerrvice.uid;
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      imageFile: File;
+      processImageURL: string;
+    },
+    private authSerrvice: AuthService,
+    private recipeService: RecipeService,
+    private dialogRef: MatDialogRef<RecipeProcessImageComponent>
+  ) {}
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+  }
+  async uploadImage(userId: string): Promise<void> {
+    const file: Blob = base64ToFile(this.croppedImage);
+    const downloadURL: string = await this.recipeService.uploadProcessImage(
+      userId,
+      file
+    );
+    this.dialogRef.close(downloadURL);
+  }
+  cancel() {
+    this.dialogRef.close(this.data.processImageURL);
+  }
+  deleteImage() {
+    this.dialogRef.close(null);
+  }
+
+  ngOnInit(): void {}
+}
