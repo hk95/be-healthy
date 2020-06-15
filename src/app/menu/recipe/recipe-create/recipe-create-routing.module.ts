3884040f81b80064d7f34dcc2ecf6fd4efef2bf2
Hideki Kamakura <kakihk695@gmail.com>
2020-06-15 21:38:54 +0900

    feat(recipe-create-routing):離脱ガード機能追加　#79

--- src/app/menu/recipe/recipe-create/recipe-create-routing.module.ts
+++ src/app/menu/recipe/recipe-create/recipe-create-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipeCreateComponent } from './recipe-create.component';
+import { RecipeFormGuard } from 'src/app/guards/recipe-form.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: RecipeCreateComponent,
+    canDeactivate: [RecipeFormGuard],
   },
 ];
 
