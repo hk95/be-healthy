76b6dc2bc664be575c37ea1114740c344f43ed2e
Hideki Kamakura <kakihk695@gmail.com>
2020-06-15 21:41:10 +0900

    feat(recipe-service):recipe-create機能追加　#79

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,11 +1,42 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AddedFood } from '../interfaces/added-food';
+import { firestore } from 'firebase';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
-  constructor(public storage: AngularFireStorage) {}
+  constructor(
+    private db: AngularFirestore,
+    public storage: AngularFireStorage,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
+
+  createRecipe(
+    recipe: Omit<AddedFood, 'recipeId' | 'processes' | 'craetedAt'>,
+    processes
+  ): Promise<void> {
+    const recipeId = this.db.createId();
+    return this.db
+      .doc<AddedFood>(`recipes/${recipeId}`)
+      .set({
+        recipeId,
+        ...recipe,
+        processes,
+        craetedAt: firestore.Timestamp.now(),
+      })
+      .then(() => {
+        this.snackBar.open('レシピを作成しました', null, {
+          duration: 2000,
+        });
+        this.router.navigateByUrl('menu');
+      });
+  }
 
   async uploadThumbnail(userId: string, file: Blob) {
     const result = await this.storage
