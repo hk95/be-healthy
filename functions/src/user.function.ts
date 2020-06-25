eeaf9d3cd4b1f34a657c4ee411615fd54d345782
Hideki Kamakura <kakihk695@gmail.com>
2020-06-25 16:42:03 +0900

    feat(user.function):indexと別別ページに表記　#55

---
+++ functions/src/user.function.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const createUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate((user) => {
+    return db.doc(`users/${user.uid}`).set({
+      name: user.displayName,
+      avatarURL: user.photoURL,
+      email: user.email,
+      createdAt: new Date(),
+      userId: user.uid,
+    });
+  });
