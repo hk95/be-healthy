9b3891870bd7abd95c1b79cae391bd6379f5ea20
Hideki Kamakura <kakihk695@gmail.com>
2020-07-02 12:27:07 +0900

    feat(set-create-routing):フォームガード実装　#104

--- src/app/menu/set/set-create/set-create-routing.module.ts
+++ src/app/menu/set/set-create/set-create-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SetCreateComponent } from './set-create.component';
+import { RecipeFormGuard } from 'src/app/guards/recipe-form.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: SetCreateComponent,
+    canDeactivate: [RecipeFormGuard],
   },
 ];
 
