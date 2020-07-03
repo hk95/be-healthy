c78d981217483afb3867c27f463aac8352ce92b2
Hideki Kamakura <kakihk695@gmail.com>
2020-07-03 11:59:15 +0900

    feat(set.component):DBから取得したデータの一覧表示　#112

--- src/app/menu/set/set.component.ts
+++ src/app/menu/set/set.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SetService } from 'src/app/services/set.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { Set } from 'src/app/interfaces/set';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-set',
@@ -9,7 +13,22 @@ import { Router } from '@angular/router';
 })
 export class SetComponent implements OnInit {
   setId: string;
-  constructor(private setService: SetService, private router: Router) {}
+  sets$: Observable<Set[]> = this.setService
+    .getSets(this.authService.uid)
+    .pipe(take(1));
+
+  constructor(
+    private setService: SetService,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.setService
+      .getSets(this.authService.uid)
+      .pipe(take(1))
+      .subscribe((v) => {
+        return v.map((s) => console.log(s));
+      });
+  }
   forwardbackToForm() {
     this.setId = this.setService.getTentativeRecipeId();
     this.router.navigate(['/set-create'], {
