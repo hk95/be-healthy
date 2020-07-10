507b6b22f7dc7545e667e46387030b216219c6cb
Hideki Kamakura <kakihk695@gmail.com>
2020-07-10 17:48:52 +0900

    feat(editor-meal-comp):食事登録、表示、削除機能実装　#68

--- src/app/editor-meal/editor-meal/editor-meal.component.ts
+++ src/app/editor-meal/editor-meal/editor-meal.component.ts
@@ -7,9 +7,11 @@ import { FoodService } from 'src/app/services/food.service';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { ActivatedRoute } from '@angular/router';
-import { BreakfastWithMeal } from 'src/app/interfaces/daily-info';
+import { DailyMealWithSet, DailyMeal } from 'src/app/interfaces/daily-info';
 import { map, take } from 'rxjs/operators';
 import { MainShellService } from 'src/app/services/main-shell.service';
+import { SetService } from 'src/app/services/set.service';
+import { Set } from 'src/app/interfaces/set';
 
 @Component({
   selector: 'app-editor-meal',
@@ -21,62 +23,70 @@ export class EditorMealComponent implements OnInit {
   @Input() Food: Food;
   amout = {};
   date: string;
-  selectedFoods$: Observable<BreakfastWithMeal[]>;
-  totalCal$: Observable<number>;
+
   favFoods$: Observable<Food[]>;
   favFoods: Food[];
   isLikedlist = [];
+  sets$: Observable<Set[]> = this.setService.getSetsOfMeal(
+    this.authService.uid,
+    'breakfast'
+  );
+  sets: Set[];
+  setList = [];
+  selectedFoods$: Observable<DailyMeal[]>;
+  selectedSets$: Observable<DailyMealWithSet[]>;
+  totalCal$: Observable<number>;
   constructor(
     private location: Location,
     private dailyInfoService: DailyInfoService,
     private foodService: FoodService,
     private authService: AuthService,
     private route: ActivatedRoute,
-    private mainShellService: MainShellService
+    private mainShellService: MainShellService,
+    private setService: SetService
   ) {
     this.route.paramMap.subscribe((paramMap) => {
       this.date = paramMap.get('date');
-      this.selectedFoods$ = this.dailyInfoService.getDailyInfoBreakfast(
-        this.authService.uid,
-        this.date
-      );
       this.mainShellService.setTitle(this.date);
     });
     this.mainShellService.setTitleMeal('朝食');
 
-    this.totalCal$ = this.selectedFoods$.pipe(
-      map((breakfastFood) =>
-        breakfastFood.reduce(
-          (total, foodCal) =>
-            total + foodCal.meal.foodCalPerAmount * foodCal.amount,
-          0
-        )
-      )
-    );
     this.favFoods$ = this.foodService.getFavFoods(this.authService.uid);
     this.favFoods$.pipe(take(1)).subscribe((foods: Food[]) => {
-      console.log(foods);
       this.favFoods = foods;
       this.isLikedlist = [...new Set(foods.map((food) => food.foodId))];
     });
-  }
-
-  addDailyInfoFood(amount: number, foodId: string) {
-    this.dailyInfoService.addDailyInfoBreakfast({
-      date: this.date,
-      breakfast: {
-        breakfastId: '',
-        amount,
-        foodId,
-      },
-      authorId: this.authService.uid,
+    this.sets$.pipe(take(1)).subscribe((sets: Set[]) => {
+      this.sets = sets;
+      this.setList = [...new Set(sets.map((set) => set.setId))];
     });
   }
-  deleteDailyInfoFood(breakfastId: string) {
-    this.dailyInfoService.deleteDailyInfoFood(
+
+  addSet(amount: number, setId: string) {
+    const meal: DailyMeal = { mealId: '', setId, amount };
+    this.dailyInfoService.addSet(
+      meal,
       this.authService.uid,
       this.date,
-      breakfastId
+      'breakfast'
+    );
+  }
+  addFood(amount: number, food: Food) {
+    const meal: DailyMeal = { mealId: '', food, amount };
+    this.dailyInfoService.addFood(
+      meal,
+      this.authService.uid,
+      this.date,
+      'breakfast'
+    );
+  }
+  deleteMeal(mealId: string, foodOrSet: string) {
+    this.dailyInfoService.deleteMeal(
+      this.authService.uid,
+      this.date,
+      mealId,
+      'breakfast',
+      foodOrSet
     );
   }
 
@@ -88,9 +98,39 @@ export class EditorMealComponent implements OnInit {
       this.favFoods.splice(index, 1);
     }
   }
+  outSet(setId: string) {
+    this.setService.updateMeal(this.authService.uid, setId, 'breakfast', false);
+    const index = this.setList.indexOf(setId);
+    if (index > -1) {
+      this.setList.splice(index, 1);
+      this.sets.splice(index, 1);
+    }
+  }
   back(): void {
     this.location.back();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.selectedFoods$ = this.dailyInfoService.getSelectedFoods(
+      this.authService.uid,
+      this.date,
+      'breakfast'
+    );
+    this.selectedSets$ = this.dailyInfoService.getSelectedSets(
+      this.authService.uid,
+      this.date,
+      'breakfast'
+    );
+    // this.totalCal$ = this.selectedFoodsOrSets$.pipe(
+    //   map((FoodOrSet) =>
+    //     FoodOrSet.reduce(
+    //       (total, foodCal) =>
+    //         total +
+    //         foodCal.food.foodCalPerAmount * foodCal.amount +
+    //         foodCal.set.setCal * foodCal.amount,
+    //       0
+    //     )
+    //   )
+    // );
+  }
 }
