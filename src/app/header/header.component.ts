6e8afdc0671bfbcd5ef8a07b6e4c13061f181340
Hideki Kamakura <kakihk695@gmail.com>
2020-07-13 16:09:40 +0900

    feat(header):食事選択見た目実装　#69

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,6 +15,7 @@ export class HeaderComponent implements OnInit {
   titleMeal$: Observable<string> = this.mainShellService.titleMeal$;
   today: string = this.getDate();
   avatarURL: string;
+  selectedValue = 'breakfast';
   constructor(
     private datepipe: DatePipe,
     private mainShellService: MainShellService,
