f2b3922a1523f2e8b0aa6ac7f85d1d170edd6548
Hideki Kamakura <kakihk695@gmail.com>
2020-07-16 10:24:24 +0900

    feat(food-search.comp):食事毎の切り替え機能実装　#69

--- src/app/editor-meal/food-search/food-search.component.ts
+++ src/app/editor-meal/food-search/food-search.component.ts
@@ -3,10 +3,11 @@ import { FoodService } from 'src/app/services/food.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { DailyInfoService } from 'src/app/services/daily-info.service';
 import { ActivatedRoute } from '@angular/router';
-import { EditorMealComponent } from '../editor-meal/editor-meal.component';
 import { SearchService } from 'src/app/services/search.service';
 import { Food } from 'src/app/interfaces/food';
 import { DailyMeal } from 'src/app/interfaces/daily-info';
+import { Observable } from 'rxjs';
+import { FavFood } from 'src/app/interfaces/fav-food';
 
 @Component({
   selector: 'app-food-search',
@@ -14,47 +15,45 @@ import { DailyMeal } from 'src/app/interfaces/daily-info';
   styleUrls: ['./food-search.component.scss'],
 })
 export class FoodSearchComponent implements OnInit {
-  amout = {};
+  amount = {};
   date: string;
+  meal: string;
+  favFoods$: Observable<FavFood[]> = this.foodService.getFavFoods(
+    this.authService.uid
+  );
+  isLikedlist = [];
   config = this.searchService.config;
 
   constructor(
-    public editorBreakfastComponent: EditorMealComponent,
     private foodService: FoodService,
     private authService: AuthService,
     private dailyInfoService: DailyInfoService,
     private route: ActivatedRoute,
     private searchService: SearchService
   ) {
-    this.route.paramMap.subscribe((date) => {
-      this.date = date.get('date');
+    this.route.queryParamMap.subscribe((paramMaps) => {
+      this.date = paramMaps.get('date');
+      this.meal = paramMaps.get('meal');
+    });
+    this.favFoods$.subscribe((foods: Food[]) => {
+      this.isLikedlist = [...new Set(foods.map((food) => food.foodId))];
     });
   }
 
   likeFavFood(foodId: string) {
     this.foodService.likeFavFood(this.authService.uid, foodId);
-    this.editorBreakfastComponent.isLikedlist.push(foodId);
-    this.foodService.getFoodByFoodId(foodId).subscribe((food) => {
-      const favFood = food;
-      this.editorBreakfastComponent.favFoods.push(favFood);
-    });
+    this.isLikedlist.push(foodId);
   }
   unLikeFavFood(foodId: string) {
     this.foodService.unLikeFavFood(this.authService.uid, foodId);
-    const index = this.editorBreakfastComponent.isLikedlist.indexOf(foodId);
+    const index = this.isLikedlist.indexOf(foodId);
     if (index > -1) {
-      this.editorBreakfastComponent.isLikedlist.splice(index, 1);
-      this.editorBreakfastComponent.favFoods.splice(index, 1);
+      this.isLikedlist.splice(index, 1);
     }
   }
   addFood(amount: number, food: Food) {
     const meal: DailyMeal = { mealId: '', food, amount };
-    this.dailyInfoService.addMeal(
-      meal,
-      this.authService.uid,
-      this.date,
-      'breakfast'
-    );
+    this.dailyInfoService.addMeal(meal, this.authService.uid, this.date);
   }
 
   ngOnInit(): void {}
