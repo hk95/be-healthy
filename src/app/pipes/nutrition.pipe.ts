f39ae81abad5b743534893cbcb5eaf65665a775c
Hideki Kamakura <kakihk695@gmail.com>
2020-07-17 17:24:38 +0900

    feat(nutrition.pipe):計算を関数化　#54

--- src/app/pipes/nutrition.pipe.ts
+++ src/app/pipes/nutrition.pipe.ts
@@ -13,243 +13,148 @@ export class NutritionPipe implements PipeTransform {
     meals3?: DailyMealWithSet[]
   ) {
     let totalResult = 0;
+    const calculation = (amount: number, value: number) => {
+      return (totalResult =
+        Math.round((totalResult + amount * value) * 10) / 10);
+    };
 
     if (meals) {
       if (whichMeal === 'all' && meals2 && meals3) {
         if (nutrition === 'cal') {
           meals.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodCalPerAmount * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodCalPerAmount);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round((totalResult + meal.set.setCal * meal.amount) * 10) /
-                10;
+              calculation(meal.amount, meal.set.setCal);
             }
           });
           meals2.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodCalPerAmount * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodCalPerAmount);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round((totalResult + meal.set.setCal * meal.amount) * 10) /
-                10;
+              calculation(meal.amount, meal.set.setCal);
             }
           });
           meals3.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodCalPerAmount * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodCalPerAmount);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round((totalResult + meal.set.setCal * meal.amount) * 10) /
-                10;
+              calculation(meal.amount, meal.set.setCal);
             }
           });
         }
         if (nutrition === 'protein') {
           meals.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodProtein * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodProtein);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setProtein * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setProtein);
             }
           });
           meals2.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodProtein * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodProtein);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setProtein * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setProtein);
             }
           });
           meals3.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodProtein * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodProtein);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setProtein * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setProtein);
             }
           });
         }
         if (nutrition === 'fat') {
           meals.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodFat * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodFat);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round((totalResult + meal.set.setFat * meal.amount) * 10) /
-                10;
+              calculation(meal.amount, meal.set.setFat);
             }
           });
           meals2.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodFat * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodFat);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round((totalResult + meal.set.setFat * meal.amount) * 10) /
-                10;
+              calculation(meal.amount, meal.set.setFat);
+            }
+          });
+          meals3.forEach((meal) => {
+            if (meal.food.foodId) {
+              calculation(meal.amount, meal.food.foodFat);
+            } else if (meal.set.setId) {
+              calculation(meal.amount, meal.set.setFat);
             }
           });
         }
         if (nutrition === 'totalCarbohydrate') {
           meals.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult +
-                    meal.food.foodTotalCarbohydrate * meal.amount) *
-                    10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodTotalCarbohydrate);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setTotalCarbohydrate * meal.amount) *
-                    10
-                ) / 10;
+              calculation(meal.amount, meal.set.setTotalCarbohydrate);
             }
           });
           meals2.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult +
-                    meal.food.foodTotalCarbohydrate * meal.amount) *
-                    10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodTotalCarbohydrate);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setTotalCarbohydrate * meal.amount) *
-                    10
-                ) / 10;
+              calculation(meal.amount, meal.set.setTotalCarbohydrate);
             }
           });
           meals3.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult +
-                    meal.food.foodTotalCarbohydrate * meal.amount) *
-                    10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodTotalCarbohydrate);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setTotalCarbohydrate * meal.amount) *
-                    10
-                ) / 10;
+              calculation(meal.amount, meal.set.setTotalCarbohydrate);
             }
           });
         }
         if (nutrition === 'dietaryFiber') {
           meals.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodDietaryFiber * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodDietaryFiber);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setDietaryFiber * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setDietaryFiber);
             }
           });
           meals2.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodDietaryFiber * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodDietaryFiber);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setDietaryFiber * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setDietaryFiber);
             }
           });
           meals3.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodDietaryFiber * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodDietaryFiber);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setDietaryFiber * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setDietaryFiber);
             }
           });
         }
         if (nutrition === 'sugar') {
           meals.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodSugar * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodSugar);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setSugar * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setSugar);
             }
           });
           meals2.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodSugar * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodSugar);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setSugar * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setSugar);
             }
           });
           meals3.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodSugar * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodSugar);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.set.setSugar * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.set.setSugar);
             }
           });
         }
@@ -258,14 +163,9 @@ export class NutritionPipe implements PipeTransform {
         if (nutrition === 'cal') {
           meals.forEach((meal) => {
             if (meal.food.foodId) {
-              totalResult =
-                Math.round(
-                  (totalResult + meal.food.foodCalPerAmount * meal.amount) * 10
-                ) / 10;
+              calculation(meal.amount, meal.food.foodCalPerAmount);
             } else if (meal.set.setId) {
-              totalResult =
-                Math.round((totalResult + meal.set.setCal * meal.amount) * 10) /
-                10;
+              calculation(meal.amount, meal.set.setCal);
             }
           });
         }
