c6bc3609c333e1324ec05a9cc3a85c72155d5af5
Hideki Kamakura <kakihk695@gmail.com>
2020-07-22 16:16:02 +0900

    feat(pfc-balance):PFC計算パイプ作成　#11

---
+++ src/app/pipes/pfc-balance.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PfcBalancePipe } from './pfc-balance.pipe';
+
+describe('PfcBalancePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PfcBalancePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
