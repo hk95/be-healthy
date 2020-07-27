402b977a67d1ec0d1d809e66dd693df93552e841
Hideki Kamakura <kakihk695@gmail.com>
2020-07-27 12:02:14 +0900

    feat(top):fullcalendar実装　#10

--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
+
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 import { MainShellService } from 'src/app/services/main-shell.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top',
@@ -8,10 +13,40 @@ import { MainShellService } from 'src/app/services/main-shell.service';
   styleUrls: ['./top.component.scss'],
 })
 export class TopComponent implements OnInit {
-  calendarPlugins = [dayGridPlugin];
+  date: string;
+  weelList = ['(日)', '(月)', '(火)', '(水)', '(木)', '(金)', '(土)'];
 
-  constructor(private mainShellService: MainShellService) {
+  calendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    weekends: true,
+    theme: 'standart',
+    headerToolbar: {
+      left: 'prev',
+      center: 'title',
+      right: 'next',
+    },
+    locale: 'ja',
+    businessHours: true,
+    dateClick: this.handleDateClick.bind(this),
+  };
+
+  constructor(
+    private mainShellService: MainShellService,
+    private router: Router
+  ) {
     this.mainShellService.setTitle('TOP');
+  }
+  handleDateClick(arg) {
+    const week = this.weelList[arg.dayEl.cellIndex];
+    this.date = arg.dateStr.slice(2).replace(/-/g, '.').replace(/$/, week);
+    console.log(this.date);
+
+    this.router.navigate(['/daily-detail'], {
+      queryParams: {
+        date: this.date,
+      },
+    });
   }
 
   ngOnInit(): void {}
