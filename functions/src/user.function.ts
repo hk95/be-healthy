cef9c342f72a10e7b29d697c81630f36817b54c8
Hideki Kamakura <kakihk695@gmail.com>
2020-07-28 16:24:28 +0900

    fix(user.fun.ts):index.tsに統一のため削除　#25

--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -1,7 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-admin.initializeApp();
 const db = admin.firestore();
 
 export const createUser = functions
