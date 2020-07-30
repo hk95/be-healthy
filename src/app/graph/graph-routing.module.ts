541f15252b2c28e4dcf863987583e1e0df5e63ee
Hideki Kamakura <kakihk695@gmail.com>
2020-07-30 15:49:41 +0900

    feat(graph-routing):グラフページパス追加　#39

---
+++ src/app/graph/graph-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { GraphComponent } from './graph/graph.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: GraphComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class GraphRoutingModule {}
