85ed8341fc1276ed167e26a9b0e93ffecea0d613
Hideki Kamakura <kakihk695@gmail.com>
2020-07-31 10:05:29 +0900

    refactor(meal.function):if文をswitch文に変更 #25

--- functions/src/meal.function.ts
+++ functions/src/meal.function.ts
@@ -9,26 +9,28 @@ export const addMeal = functions
     const increment = admin.firestore.FieldValue.increment(
       data.amount * data.cal
     );
-    if (data.meal === 'breakfast') {
-      return db
-        .doc(`users/${data.userId}/dailyInfos/${data.date}`)
-        .set({ totalCal: increment, breakfastCal: increment }, { merge: true })
-        .catch(() => console.log('success'))
-        .catch((err) => console.log(err));
-    }
-    if (data.meal === 'lunch') {
-      return db
-        .doc(`users/${data.userId}/dailyInfos/${data.date}`)
-        .set({ totalCal: increment, lunchCal: increment }, { merge: true })
-        .catch(() => console.log('success'))
-        .catch((err) => console.log(err));
-    }
-    if (data.meal === 'dinner') {
-      return db
-        .doc(`users/${data.userId}/dailyInfos/${data.date}`)
-        .set({ totalCal: increment, dinnerCal: increment }, { merge: true })
-        .catch(() => console.log('success'))
-        .catch((err) => console.log(err));
+    switch (data.meal) {
+      case 'breakfast':
+        db.doc(`users/${data.userId}/dailyInfos/${data.date}`)
+          .set(
+            { totalCal: increment, breakfastCal: increment },
+            { merge: true }
+          )
+          .catch(() => console.log('success'))
+          .catch((err) => console.log(err));
+        break;
+      case 'lunch':
+        db.doc(`users/${data.userId}/dailyInfos/${data.date}`)
+          .set({ totalCal: increment, lunchCal: increment }, { merge: true })
+          .catch(() => console.log('success'))
+          .catch((err) => console.log(err));
+        break;
+      case 'dinner':
+        db.doc(`users/${data.userId}/dailyInfos/${data.date}`)
+          .set({ totalCal: increment, dinnerCal: increment }, { merge: true })
+          .catch(() => console.log('success'))
+          .catch((err) => console.log(err));
+        break;
     }
   });
 export const removeMeal = functions
@@ -37,25 +39,24 @@ export const removeMeal = functions
     const decline = admin.firestore.FieldValue.increment(
       -data.amount * data.cal
     );
-    if (data.meal === 'breakfast') {
-      return db
-        .doc(`users/${data.userId}/dailyInfos/${data.date}`)
-        .update({ totalCal: decline, breakfastCal: decline })
-        .catch(() => console.log('success'))
-        .catch((err) => console.log(err));
-    }
-    if (data.meal === 'lunch') {
-      return db
-        .doc(`users/${data.userId}/dailyInfos/${data.date}`)
-        .update({ totalCal: decline, lunchCal: decline })
-        .catch(() => console.log('success'))
-        .catch((err) => console.log(err));
-    }
-    if (data.meal === 'dinner') {
-      return db
-        .doc(`users/${data.userId}/dailyInfos/${data.date}`)
-        .update({ totalCal: decline, dinnerCal: decline })
-        .catch(() => console.log('success'))
-        .catch((err) => console.log(err));
+    switch (data.meal) {
+      case 'breakfast':
+        db.doc(`users/${data.userId}/dailyInfos/${data.date}`)
+          .update({ totalCal: decline, breakfastCal: decline })
+          .catch(() => console.log('success'))
+          .catch((err) => console.log(err));
+        break;
+      case 'lunch':
+        db.doc(`users/${data.userId}/dailyInfos/${data.date}`)
+          .update({ totalCal: decline, lunchCal: decline })
+          .catch(() => console.log('success'))
+          .catch((err) => console.log(err));
+        break;
+      case 'dinner':
+        db.doc(`users/${data.userId}/dailyInfos/${data.date}`)
+          .update({ totalCal: decline, dinnerCal: decline })
+          .catch(() => console.log('success'))
+          .catch((err) => console.log(err));
+        break;
     }
   });
