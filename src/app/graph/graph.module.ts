022713d4ab3a8e6503d492f3e162a0814effe595
Hideki Kamakura <kakihk695@gmail.com>
2020-08-04 10:07:33 +0900

    feat(graph.module):mat-datepickerインストール #39

--- src/app/graph/graph.module.ts
+++ src/app/graph/graph.module.ts
@@ -4,9 +4,24 @@ import { CommonModule } from '@angular/common';
 import { GraphRoutingModule } from './graph-routing.module';
 import { GraphComponent } from './graph/graph.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DatePipe } from '@angular/common';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [GraphComponent],
-  imports: [CommonModule, GraphRoutingModule, NgxChartsModule],
+  imports: [
+    CommonModule,
+    GraphRoutingModule,
+    NgxChartsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  providers: [DatePipe],
 })
 export class GraphModule {}
