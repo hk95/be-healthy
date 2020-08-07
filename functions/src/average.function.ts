26ca167692fbc63908e0fe6e8a689e87042d7038
Hideki Kamakura <kakihk695@gmail.com>
2020-08-07 17:25:28 +0900

    feat(average.func):関数の統合 #39

--- functions/src/average.function.ts
+++ functions/src/average.function.ts
@@ -3,252 +3,190 @@ import * as admin from 'firebase-admin';
 
 const db = admin.firestore();
 
-export const bodyOfYear = functions
+const aggregateAverageOfYear = (
+  whichCategory: string,
+  userId: string,
+  year: number
+) => {
+  let totalCount = 0;
+  let totalValue = 0;
+  db.doc(`users/${userId}/averagesYear/${year}年${whichCategory}`)
+    .get()
+    .then((doc) => {
+      for (let i = 1; i <= 366; i++) {
+        const key: string = i.toString();
+        if (doc.data()?.[key] || doc.data()?.[key] === 0) {
+          totalCount += 1;
+          totalValue += doc.data()?.[key];
+        }
+      }
+      if (totalCount > 0) {
+        const averageOfYear = totalValue / totalCount;
+        return db
+          .doc(`users/${userId}/averagesYear/${year}年${whichCategory}`)
+          .set({ averageOfYear }, { merge: true });
+      } else {
+        return db
+          .doc(`users/${userId}/averagesYear/${year}年${whichCategory}`)
+          .set({ averageOfYear: 0 }, { merge: true });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+const aggregateAverageOfMonth = (
+  whichCategory: string,
+  userId: string,
+  year: number,
+  month: number
+) => {
+  let totalCount = 0;
+  let totalValue = 0;
+  db.doc(`users/${userId}/averagesMonth/${year}年${month}月${whichCategory}`)
+    .get()
+    .then((doc) => {
+      for (let i = 1; i <= 31; i++) {
+        const key: string = i.toString();
+        if (doc.data()?.[key] || doc.data()?.[key] === 0) {
+          totalCount += 1;
+          totalValue += doc.data()?.[key];
+        }
+      }
+      if (totalCount > 0) {
+        const averageOfMonth = totalValue / totalCount;
+        return db
+          .doc(
+            `users/${userId}/averagesMonth/${year}年${month}月${whichCategory}`
+          )
+          .set({ averageOfMonth }, { merge: true });
+      } else {
+        return db
+          .doc(
+            `users/${userId}/averagesMonth/${year}年${month}月${whichCategory}`
+          )
+          .set({ averageOfMonth: 0 }, { merge: true });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+const aggregateAverageOfWeek = (
+  whichCategory: string,
+  userId: string,
+  year: number,
+  week: number
+) => {
+  let totalCount = 0;
+  let totalValue = 0;
+  db.doc(`users/${userId}/averagesWeek/${year}年${week}週目${whichCategory}`)
+    .get()
+    .then((doc) => {
+      for (let i = 0; i <= 6; i++) {
+        const key: string = i.toString();
+        if (doc.data()?.[key] || doc.data()?.[key] === 0) {
+          totalCount += 1;
+          totalValue += doc.data()?.[key];
+        }
+      }
+      if (totalCount > 0) {
+        const averageOfWeek = totalValue / totalCount;
+        return db
+          .doc(
+            `users/${userId}/averagesWeek/${year}年${week}週目${whichCategory}`
+          )
+          .set({ averageOfWeek }, { merge: true });
+      } else {
+        return db
+          .doc(
+            `users/${userId}/averagesWeek/${year}年${week}週目${whichCategory}`
+          )
+          .set({ averageOfWeek: 0 }, { merge: true });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+export const averageWeightOfYear = functions
   .region('asia-northeast1')
   .runWith({
     timeoutSeconds: 540,
     memory: '2GB',
   })
   .https.onCall((data) => {
-    let totalCount = 0;
-    let totalValue = 0;
-    const aggregateAverage = (whichCategory: string) => {
-      db.doc(`users/${data.userId}/averagesYear/${data.year}年${whichCategory}`)
-        .get()
-        .then((doc) => {
-          for (let i = 1; i <= 366; i++) {
-            const key: string = i.toString();
-            if (doc.data()?.[key] || doc.data()?.[key] === 0) {
-              totalCount += 1;
-              totalValue += doc.data()?.[key];
-            }
-          }
-          if (totalCount > 0) {
-            const averageOfYear = totalValue / totalCount;
-            return db
-              .doc(
-                `users/${data.userId}/averagesYear/${data.year}年${whichCategory}`
-              )
-              .set({ averageOfYear }, { merge: true });
-          } else {
-            return db
-              .doc(
-                `users/${data.userId}/averagesYear/${data.year}年${whichCategory}`
-              )
-              .set({ averageOfYear: 0 }, { merge: true });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    aggregateAverage('体重');
-    aggregateAverage('体脂肪');
+    aggregateAverageOfYear('体重', data.userId, data.year);
   });
-export const bodyOfMonth = functions
+export const averageFatOfYear = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 540,
+    memory: '2GB',
+  })
+  .https.onCall((data) => {
+    aggregateAverageOfYear('体脂肪', data.userId, data.year);
+  });
+export const averageCalOfYear = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 540,
+    memory: '2GB',
+  })
+  .https.onCall((data) => {
+    aggregateAverageOfYear('カロリー', data.userId, data.year);
+  });
+export const averageWeightOfMonth = functions
   .region('asia-northeast1')
   .runWith({
     timeoutSeconds: 540,
     memory: '2GB',
   })
   .https.onCall(async (data) => {
-    let totalCount = 0;
-    let totalValue = 0;
-    const aggregateAverage = (whichCategory: string) => {
-      db.doc(
-        `users/${data.userId}/averagesMonth/${data.year}年${data.month}月${whichCategory}`
-      )
-        .get()
-        .then((doc) => {
-          for (let i = 1; i <= 31; i++) {
-            const key: string = i.toString();
-            if (doc.data()?.[key] || doc.data()?.[key] === 0) {
-              totalCount += 1;
-              totalValue += doc.data()?.[key];
-            }
-          }
-          if (totalCount > 0) {
-            const averageOfMonth = totalValue / totalCount;
-            return db
-              .doc(
-                `users/${data.userId}/averagesMonth/${data.year}年${data.month}月${whichCategory}`
-              )
-              .set({ averageOfMonth }, { merge: true });
-          } else {
-            return db
-              .doc(
-                `users/${data.userId}/averagesMonth/${data.year}年${data.month}月${whichCategory}`
-              )
-              .set({ averageOfMonth: 0 }, { merge: true });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    aggregateAverage('体重');
-    aggregateAverage('体脂肪');
+    aggregateAverageOfMonth('体重', data.userId, data.year, data.month);
   });
-export const bodyOfWeek = functions
+export const averageFatOfMonth = functions
   .region('asia-northeast1')
   .runWith({
     timeoutSeconds: 540,
     memory: '2GB',
   })
   .https.onCall(async (data) => {
-    let totalCount = 0;
-    let totalValue = 0;
-    const aggregateAverage = (whichCategory: string) => {
-      db.doc(
-        `users/${data.userId}/averagesWeek/${data.year}年${data.week}週目${whichCategory}`
-      )
-        .get()
-        .then((doc) => {
-          for (let i = 0; i <= 6; i++) {
-            const key: string = i.toString();
-            if (doc.data()?.[key] || doc.data()?.[key] === 0) {
-              totalCount += 1;
-              totalValue += doc.data()?.[key];
-            }
-          }
-          if (totalCount > 0) {
-            const averageOfWeek = totalValue / totalCount;
-            return db
-              .doc(
-                `users/${data.userId}/averagesWeek/${data.year}年${data.week}週目${whichCategory}`
-              )
-              .set({ averageOfWeek }, { merge: true });
-          } else {
-            return db
-              .doc(
-                `users/${data.userId}/averagesWeek/${data.year}年${data.week}週目${whichCategory}`
-              )
-              .set({ averageOfWeek: 0 }, { merge: true });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    aggregateAverage('体重');
-    aggregateAverage('体脂肪');
+    aggregateAverageOfMonth('体脂肪', data.userId, data.year, data.month);
   });
-
-export const calOfYear = functions
+export const averageCalOfMonth = functions
   .region('asia-northeast1')
   .runWith({
     timeoutSeconds: 540,
     memory: '2GB',
   })
   .https.onCall(async (data) => {
-    let totalCount = 0;
-    let totalCal = 0;
-    return db
-      .doc(`users/${data.userId}/averagesYear/${data.year}年カロリー`)
-      .get()
-      .then((doc) => {
-        for (let i = 1; i <= 366; i++) {
-          const key: string = i.toString();
-          if (doc.data()?.[key] || doc.data()?.[key] === 0) {
-            totalCount += 1;
-            totalCal += doc.data()?.[key];
-          }
-        }
-        if (totalCount > 0) {
-          const averageOfYear = totalCal / totalCount;
-          return db
-            .doc(`users/${data.userId}/averagesYear/${data.year}年カロリー`)
-            .set({ averageOfYear }, { merge: true });
-        } else {
-          return db
-            .doc(`users/${data.userId}/averagesYear/${data.year}年カロリー`)
-            .set({ averageOfYear: 0 }, { merge: true });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    aggregateAverageOfMonth('カロリー', data.userId, data.year, data.month);
   });
-export const calOfMonth = functions
+export const averageWeightOfWeek = functions
   .region('asia-northeast1')
   .runWith({
     timeoutSeconds: 540,
     memory: '2GB',
   })
   .https.onCall(async (data) => {
-    let totalCount = 0;
-    let totalCal = 0;
-    return db
-      .doc(
-        `users/${data.userId}/averagesMonth/${data.year}年${data.month}月カロリー`
-      )
-      .get()
-      .then((doc) => {
-        for (let i = 1; i <= 31; i++) {
-          const key: string = i.toString();
-          if (doc.data()?.[key] || doc.data()?.[key] === 0) {
-            totalCount += 1;
-            totalCal += doc.data()?.[key];
-          }
-        }
-        if (totalCount > 0) {
-          const averageOfMonth = totalCal / totalCount;
-          return db
-            .doc(
-              `users/${data.userId}/averagesMonth/${data.year}年${data.month}月カロリー`
-            )
-            .set({ averageOfMonth }, { merge: true });
-        } else {
-          return db
-            .doc(
-              `users/${data.userId}/averagesMonth/${data.year}年${data.month}月カロリー`
-            )
-            .set({ averageOfMonth: 0 }, { merge: true });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    aggregateAverageOfWeek('体重', data.userId, data.year, data.week);
   });
-export const calOfWeek = functions
+export const averageFatOfWeek = functions
   .region('asia-northeast1')
   .runWith({
     timeoutSeconds: 540,
     memory: '2GB',
   })
   .https.onCall(async (data) => {
-    let totalCount = 0;
-    let totalCal = 0;
-    return db
-      .doc(
-        `users/${data.userId}/averagesWeek/${data.year}年${data.week}週目カロリー`
-      )
-      .get()
-      .then((doc) => {
-        for (let i = 0; i <= 6; i++) {
-          const key: string = i.toString();
-          if (doc.data()?.[key] || doc.data()?.[key] === 0) {
-            totalCount += 1;
-            totalCal += doc.data()?.[key];
-          }
-        }
-        if (totalCount > 0) {
-          const averageOfWeek = totalCal / totalCount;
-
-          return db
-            .doc(
-              `users/${data.userId}/averagesWeek/${data.year}年${data.week}週目カロリー`
-            )
-            .set({ averageOfWeek }, { merge: true });
-        } else {
-          return db
-            .doc(
-              `users/${data.userId}/averagesWeek/${data.year}年${data.week}週目カロリー`
-            )
-            .set({ averageOfWeek: 0 }, { merge: true });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    aggregateAverageOfWeek('体脂肪', data.userId, data.year, data.week);
+  });
+export const averageCalOfWeek = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 540,
+    memory: '2GB',
+  })
+  .https.onCall(async (data) => {
+    aggregateAverageOfWeek('体脂肪', data.userId, data.year, data.week);
   });
