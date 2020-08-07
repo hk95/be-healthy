fde2595ce8072b5daa5d0a97bc04e70c50ec47c1
Hideki Kamakura <kakihk695@gmail.com>
2020-08-07 09:46:01 +0900

    feat(average.ts):averageインターフェイス作成  #39

---
+++ src/app/interfaces/average.ts
@@ -0,0 +1,20 @@
+export interface AverageOfYear {
+  year: number;
+  date: string;
+  averageOfYear: number;
+  category: string;
+}
+export interface AverageOfMonth {
+  year: number;
+  month: number;
+  date: string;
+  averageOfMonth: number;
+  category: string;
+}
+export interface AverageOfWeek {
+  year: number;
+  week: number;
+  date: string;
+  averageOfWeek: number;
+  category: string;
+}
