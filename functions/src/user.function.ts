23c28383dc8ec7f0c5fec240f20fc12d461d20bb
Hideki Kamakura <kakihk695@gmail.com>
2020-08-13 12:51:13 +0900

    feat(user.function.ts)新規ユーザー認証時に顧客を作成 #45

--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -1,17 +1,35 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import { auth } from 'firebase-admin';
+import Stripe from 'stripe';
 
-const db = admin.firestore();
+import { db } from './db';
+import { stripe } from './stripe/client';
 
 export const createUser = functions
   .region('asia-northeast1')
   .auth.user()
-  .onCreate((user) => {
-    return db.doc(`users/${user.uid}`).set({
+  .onCreate(async (user: auth.UserRecord) => {
+    db.doc(`users/${user.uid}`)
+      .set({
+        name: user.displayName,
+        avatarURL: user.photoURL,
+        email: user.email,
+        createdAt: new Date(),
+        userId: user.uid,
+      })
+      .then(() => {
+        console.log('success');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    const customer: Stripe.Customer = await stripe.customers.create({
       name: user.displayName,
-      avatarURL: user.photoURL,
       email: user.email,
-      createdAt: new Date(),
+    });
+
+    return db.doc(`customers/${user.uid}`).set({
       userId: user.uid,
+      customerId: customer.id,
     });
   });
