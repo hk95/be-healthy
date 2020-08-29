fc088cb72a53131bce94d86c33ce30bfef4a6054
Hideki Kamakura <kakihk695@gmail.com>
2020-08-29 09:33:13 +0900

    feat(recipe.component.ts):mat-spinner導入#77

--- src/app/menu/recipe/recipe.component.ts
+++ src/app/menu/recipe/recipe.component.ts
@@ -1,28 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { RecipeWithAuthor } from 'src/app/interfaces/recipe';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { BasicInfoService } from 'src/app/services/basic-info.service';
+import { BasicInfo } from 'src/app/interfaces/basic-info';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.scss'],
 })
-export class RecipeComponent implements OnInit {
-  myRecipes$: Observable<RecipeWithAuthor[]>;
-  publicRecipes$: Observable<RecipeWithAuthor[]>;
-  userId = this.authService.uid;
+export class RecipeComponent implements OnInit, OnDestroy {
+  private myRecipeSub: Subscription;
+  private publicRecipeSub: Subscription;
+  private userId = this.authService.uid;
+
+  loading: boolean;
+  basicInfo$: Observable<BasicInfo> = this.basicInfoService.getBasicInfo(
+    this.userId
+  );
+  myRecipes: RecipeWithAuthor[];
+  publicRecipes: RecipeWithAuthor[];
+
   constructor(
     private recipeService: RecipeService,
-    private authService: AuthService
+    private authService: AuthService,
+    private basicInfoService: BasicInfoService
   ) {
-    this.myRecipes$ = this.recipeService.getMyRecipes(this.userId);
-    this.publicRecipes$ = this.recipeService.getPublicRecipes(this.userId);
+    this.loading = true;
+    this.myRecipeSub = this.recipeService
+      .getMyRecipes(this.userId)
+      .subscribe((value) => {
+        if (value && value.length > 0) {
+          this.myRecipes = value;
+        }
+        this.loading = false;
+      });
+    this.publicRecipeSub = this.recipeService
+      .getPublicRecipes(this.userId)
+      .subscribe((value) => {
+        this.publicRecipes = value;
+      });
   }
 
   forwardbackToForm() {
     this.recipeService.tentativeCreateRecipe();
   }
   ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.myRecipeSub.unsubscribe();
+    this.publicRecipeSub.unsubscribe();
+  }
 }
