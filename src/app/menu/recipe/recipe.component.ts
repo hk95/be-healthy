9c4728398c77d1db1f5389ce448e75811c4df077
Hideki Kamakura <kakihk695@gmail.com>
2020-08-31 20:08:35 +0900

    feat(recipe.component):追加読み込み、mat-spinner実装#77

--- src/app/menu/recipe/recipe.component.ts
+++ src/app/menu/recipe/recipe.component.ts
@@ -1,55 +1,96 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-import { RecipeWithAuthor } from 'src/app/interfaces/recipe';
+import { Component, OnInit } from '@angular/core';
+import { RecipeWithAuthor, Recipe } from 'src/app/interfaces/recipe';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { AuthService } from 'src/app/services/auth.service';
-import { BasicInfoService } from 'src/app/services/basic-info.service';
-import { BasicInfo } from 'src/app/interfaces/basic-info';
+
+import { QueryDocumentSnapshot } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.scss'],
 })
-export class RecipeComponent implements OnInit, OnDestroy {
-  private myRecipeSub: Subscription;
-  private publicRecipeSub: Subscription;
+export class RecipeComponent implements OnInit {
   private userId = this.authService.uid;
+  private getNumber = 10;
+  private lastMyRecipeDoc: QueryDocumentSnapshot<Recipe>;
+  private lastPublicRecipeDoc: QueryDocumentSnapshot<Recipe>;
 
-  loading: boolean;
-  basicInfo$: Observable<BasicInfo> = this.basicInfoService.getBasicInfo(
-    this.userId
-  );
-  myRecipes: RecipeWithAuthor[];
-  publicRecipes: RecipeWithAuthor[];
+  myRecipeloading: boolean;
+  publicRecipeloading: boolean;
+  myRecipes: RecipeWithAuthor[] = new Array();
+  publicRecipes: RecipeWithAuthor[] = new Array();
+  isMyRecipeNext: boolean;
+  isPublicRecipeNext: boolean;
 
   constructor(
     private recipeService: RecipeService,
-    private authService: AuthService,
-    private basicInfoService: BasicInfoService
+    private authService: AuthService
   ) {
-    this.loading = true;
-    this.myRecipeSub = this.recipeService
-      .getMyRecipes(this.userId)
-      .subscribe((value) => {
-        if (value && value.length > 0) {
-          this.myRecipes = value;
-        }
-        this.loading = false;
-      });
-    this.publicRecipeSub = this.recipeService
-      .getPublicRecipes(this.userId)
-      .subscribe((value) => {
-        this.publicRecipes = value;
-      });
+    this.getMyRecipes();
+    this.getPublicRecipes();
   }
 
   forwardbackToForm() {
     this.recipeService.tentativeCreateRecipe();
   }
-  ngOnInit(): void {}
-  ngOnDestroy(): void {
-    this.myRecipeSub.unsubscribe();
-    this.publicRecipeSub.unsubscribe();
+  getMyRecipes() {
+    this.myRecipeloading = true;
+    this.recipeService
+      .getMyRecipes(this.userId, this.getNumber, this.lastMyRecipeDoc)
+      .pipe(take(1))
+      .subscribe(
+        (doc: {
+          data: RecipeWithAuthor[];
+          nextLastDoc: QueryDocumentSnapshot<Recipe>;
+        }) => {
+          if (doc) {
+            this.lastMyRecipeDoc = doc.nextLastDoc;
+            if (doc.data && doc.data.length > 0) {
+              doc.data.forEach((recipe: RecipeWithAuthor) => {
+                this.myRecipes.push(recipe);
+                if (doc.data.length >= this.getNumber) {
+                  this.isMyRecipeNext = true;
+                } else {
+                  this.isMyRecipeNext = false;
+                }
+              });
+            }
+          } else {
+            this.isMyRecipeNext = false;
+          }
+          this.myRecipeloading = false;
+        }
+      );
   }
+  getPublicRecipes() {
+    this.publicRecipeloading = true;
+    this.recipeService
+      .getPublicRecipes(this.getNumber, this.lastPublicRecipeDoc)
+      .pipe(take(1))
+      .subscribe(
+        (doc: {
+          data: RecipeWithAuthor[];
+          nextLastDoc: QueryDocumentSnapshot<Recipe>;
+        }) => {
+          if (doc) {
+            this.lastPublicRecipeDoc = doc.nextLastDoc;
+            if (doc.data && doc.data.length > 0) {
+              doc.data.forEach((recipe: RecipeWithAuthor) => {
+                this.publicRecipes.push(recipe);
+                if (doc.data.length >= this.getNumber) {
+                  this.isPublicRecipeNext = true;
+                }
+              });
+            }
+          } else {
+            this.isPublicRecipeNext = false;
+          }
+          this.publicRecipeloading = false;
+        }
+      );
+  }
+
+  ngOnInit(): void {}
 }
