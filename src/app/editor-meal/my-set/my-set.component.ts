6deba0c7959137692540cf158552221f319f0595
Hideki Kamakura <kakihk695@gmail.com>
2020-09-03 18:02:49 +0900

    feat(my-set.component):もっと見る、mat-spinner導入#71

--- src/app/editor-meal/my-set/my-set.component.ts
+++ src/app/editor-meal/my-set/my-set.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { Set } from 'src/app/interfaces/set';
 import { DailyMeal } from 'src/app/interfaces/daily-info';
 import { DailyInfoService } from 'src/app/services/daily-info.service';
@@ -8,6 +8,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { SetService } from 'src/app/services/set.service';
 import { AverageService } from 'src/app/services/average.service';
 import { Validators, FormControl, FormBuilder } from '@angular/forms';
+import { QueryDocumentSnapshot } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-set',
@@ -15,14 +17,19 @@ import { Validators, FormControl, FormBuilder } from '@angular/forms';
   styleUrls: ['./my-set.component.scss'],
 })
 export class MySetComponent implements OnInit, OnDestroy {
+  private lastDoc: QueryDocumentSnapshot<Set>;
+
   amount = [].fill(0);
   date: string;
   meal: string;
-  sets$: Observable<Set[]>;
-  totalCal$: Observable<number>;
-  routerSub: Subscription;
+  sets: Set[] = new Array();
+  subscription = new Subscription();
   minAmount = 0;
   maxAmount = 100;
+  getNumber = 10;
+  isNext: boolean;
+  loading: boolean;
+
   amountForm = this.fb.group({
     amount: [
       0,
@@ -45,38 +52,64 @@ export class MySetComponent implements OnInit, OnDestroy {
       this.date = paramMaps.get('date');
       this.meal = paramMaps.get('meal');
     });
-    this.sets$ = this.setService.getSetsOfMeal(this.authService.uid, this.meal);
-    this.routerSub = this.router.events.subscribe((event) => {
+    this.getSets();
+    const routerSub = this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.averageService.averageTotalCal(this.authService.uid, this.date);
       }
     });
+    this.subscription.add(routerSub);
+  }
+  getSets() {
+    this.loading = true;
+    this.setService
+      .getSets(this.authService.uid, this.getNumber, this.lastDoc, this.meal)
+      .pipe(take(1))
+      .subscribe((sets) => {
+        if (sets && sets.length > 0) {
+          sets.forEach(
+            (set: { data: Set; nextLastDoc: QueryDocumentSnapshot<Set> }) => {
+              this.sets.push(set.data);
+              this.lastDoc = set.nextLastDoc;
+            }
+          );
+          if (sets.length >= this.getNumber) {
+            this.isNext = true;
+          }
+        } else {
+          this.isNext = false;
+        }
+        this.loading = false;
+      });
   }
   addSet(amount: number, set: Set) {
     const meal: DailyMeal = { mealId: '', set, amount };
     this.dailyInfoService.addMeal(meal, this.authService.uid, this.date, 'set');
   }
 
-  outSet(setId: string) {
+  outSet(setId: string, index: number) {
     this.setService.updateMeal(this.authService.uid, setId, this.meal, false);
+    this.sets.splice(index, 1);
   }
+
   goToSetPage() {
     this.dailyInfoService.goToSetPage(this.date, this.meal);
   }
 
   ngOnInit(): void {
-    this.dailyInfoService.whichMeal$.subscribe((whichMeal) => {
-      if (whichMeal !== 'notChange') {
-        this.meal = whichMeal;
-
-        this.sets$ = this.setService.getSetsOfMeal(
-          this.authService.uid,
-          this.meal
-        );
+    const changeMealSub = this.dailyInfoService.whichMeal$.subscribe(
+      (whichMeal) => {
+        if (whichMeal !== 'notChange') {
+          this.lastDoc = null;
+          this.meal = whichMeal;
+          this.sets = new Array();
+          this.getSets();
+        }
       }
-    });
+    );
+    this.subscription.add(changeMealSub);
   }
   ngOnDestroy() {
-    this.routerSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
