0395a793ef8163444eb0dd37c1fd5be8a1308341
Hideki Kamakura <kakihk695@gmail.com>
2020-09-07 20:42:00 +0900

    feat(user.service):ユーザーアカウント削除関数の作成#64

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,45 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FirebaseApp } from '@angular/fire';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private db: AngularFirestore) {}
+  constructor(
+    private db: AngularFirestore,
+    private snackBar: MatSnackBar,
+    private firebase: FirebaseApp,
+    private router: Router
+  ) {}
 
   getUser(userId: string) {
     return this.db.doc<User>(`users/${userId}`).valueChanges();
+  }
+  async deleteUserAccount(): Promise<void> {
+    this.snackBar.open('削除中...', null, {
+      duration: null,
+    });
+    return this.firebase
+      .auth()
+      .currentUser.delete()
+      .then(() => {
+        this.router.navigateByUrl('/graph');
+        this.snackBar.open(
+          'アカウントの削除が完了しました。ご利用いただきありがとうございました。',
+          null,
+          {
+            duration: 2000,
+          }
+        );
+      })
+      .catch(() => {
+        this.snackBar.open(
+          'アカウントの削除に失敗しました。お手数ですが、時間をおき再度お願いします。'
+        );
+      });
   }
 }
