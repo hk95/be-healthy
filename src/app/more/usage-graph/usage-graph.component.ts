06fba536f3a0702f38758b8966e3332ac252cd73
Hideki Kamakura <kakihk695@gmail.com>
2020-09-10 16:04:57 +0900

    feat(usage-graph):グラフ使用法実装#128

---
+++ src/app/more/usage-graph/usage-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usage-graph',
+  templateUrl: './usage-graph.component.html',
+  styleUrls: ['./usage-graph.component.scss'],
+})
+export class UsageGraphComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
