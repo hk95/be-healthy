c0409f68ec175d867339d462af55922903cdb7e5
Hideki Kamakura <kakihk695@gmail.com>
2020-09-11 17:24:41 +0900

    feat(top.component):初回ログイン処理実装#128

--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
 
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -8,6 +7,11 @@ import { MainShellService } from 'src/app/services/main-shell.service';
 import { Router } from '@angular/router';
 import { DailyInfoService } from 'src/app/services/daily-info.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { TutorialComponent } from 'src/app/dialogs/tutorial/tutorial.component';
+import { BasicInfoService } from 'src/app/services/basic-info.service';
+import { BasicInfo } from 'src/app/interfaces/basic-info';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-top',
@@ -32,15 +36,31 @@ export class TopComponent implements OnInit {
     businessHours: true,
     dateClick: this.handleDateClick.bind(this),
   };
+  basicInfo$: Observable<BasicInfo>;
 
   constructor(
     private mainShellService: MainShellService,
     private router: Router,
     private dailyInfoService: DailyInfoService,
-    private authService: AuthService
+    private authService: AuthService,
+    private dialog: MatDialog,
+    private basicInfoService: BasicInfoService
   ) {
+    this.isInitLogin();
     this.mainShellService.setTitle('TOP');
   }
+
+  isInitLogin() {
+    if (!this.authService.isInitialLogin) {
+      this.basicInfo$ = this.basicInfoService.getBasicInfo(
+        this.authService.uid
+      );
+      this.dialog.open(TutorialComponent, {
+        width: '100%',
+      });
+    }
+  }
+
   handleDateClick(arg) {
     const week = this.weelList[arg.dayEl.cellIndex];
     this.date = arg.dateStr
