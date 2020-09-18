b21f6a0b97ec96293065fa4d677f56963b47c1c4
Hideki Kamakura <kakihk695@gmail.com>
2020-09-18 10:38:57 +0900

    feat(top.component.ts):カレンダー未来の日付選択不可、登録有無の表示#100

--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -11,19 +11,23 @@ import { MatDialog } from '@angular/material/dialog';
 import { TutorialComponent } from 'src/app/dialogs/tutorial/tutorial.component';
 import { BasicInfoService } from 'src/app/services/basic-info.service';
 import { BasicInfo } from 'src/app/interfaces/basic-info';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { DatePipe } from '@angular/common';
+import { DailyInfoList } from 'src/app/interfaces/daily-info';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
   styleUrls: ['./top.component.scss'],
 })
-export class TopComponent implements OnInit {
+export class TopComponent implements OnInit, OnDestroy {
   private readonly weekList = ['日', '月', '火', '水', '木', '金', '土'];
+  private readonly today: string = this.getDate();
   private date: string;
-  today: string = this.getDate();
+  private isDataList = [];
+  private subscription: Subscription;
 
+  minDate = this.getDate(new Date(2018, 0, 1));
   calendarOptions = {
     plugins: [dayGridPlugin, interactionPlugin],
     initialView: 'dayGridMonth',
@@ -34,9 +38,10 @@ export class TopComponent implements OnInit {
       center: 'title',
       right: 'next',
     },
+    events: [],
     locale: 'ja',
-    validRange: {
-      start: '2018-01-01',
+    dayCellContent(event) {
+      event.dayNumberText = event.dayNumberText.replace('日', '');
     },
     businessHours: true,
     dateClick: this.handleDateClick.bind(this),
@@ -54,6 +59,7 @@ export class TopComponent implements OnInit {
   ) {
     this.isInitLogin();
     this.mainShellService.setTitle('TOP');
+    this.getDailyInfos();
   }
 
   isInitLogin() {
@@ -66,13 +72,46 @@ export class TopComponent implements OnInit {
     }
   }
 
+  getDailyInfos() {
+    this.subscription = this.dailyInfoService
+      .getDailyInfosOfMonths(this.authService.uid, this.today)
+      .subscribe((v: DailyInfoList[]) => {
+        v.forEach((monthData: DailyInfoList) => {
+          for (let i = 1; i <= 31; i++) {
+            if (monthData.list[i]) {
+              const correctedDate = monthData.list[i].date
+                .replace(/\./g, '-')
+                .substr(0, 8)
+                .replace(/^/, '20');
+              console.log(correctedDate);
+              if (monthData.list[i].currentWeight) {
+                this.isDataList.push({
+                  date: correctedDate,
+                  display: 'list-item',
+                  color: '#1976D2',
+                });
+              }
+              if (monthData.list[i].totalCal) {
+                this.isDataList.push({
+                  date: correctedDate,
+                  display: 'list-item',
+                  color: '#dbdbdb',
+                });
+              }
+            }
+            this.calendarOptions.events = this.isDataList;
+          }
+        });
+      });
+  }
+
   handleDateClick(arg) {
     const week = this.weekList[arg.dayEl.cellIndex];
     this.date = arg.dateStr
       .slice(2)
       .replace(/-/g, '.')
       .replace(/$/, '(' + week + ')');
-    if (this.today >= this.date) {
+    if (this.today >= this.date && this.minDate <= this.date) {
       this.dailyInfoService.createDailyInfo({
         authorId: this.authService.uid,
         date: this.date,
@@ -85,10 +124,18 @@ export class TopComponent implements OnInit {
     }
   }
 
-  getDate(): string {
-    const d = new Date();
-    return this.datePipe.transform(d, 'yy.MM.dd(E)');
+  getDate(event?: Date): string {
+    if (event) {
+      return this.datePipe.transform(event, 'yy.MM.dd(E)');
+    } else {
+      const d = new Date();
+      return this.datePipe.transform(d, 'yy.MM.dd(E)');
+    }
   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
