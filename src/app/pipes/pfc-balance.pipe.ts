d45a7e5299dc8e995b8ef890b3491287e62ebdf5
Hideki Kamakura <kakihk695@gmail.com>
2020-09-19 11:51:34 +0900

    fix(pfc.pipe):セット、foodそれぞれの例外処理変更#100

--- src/app/pipes/pfc-balance.pipe.ts
+++ src/app/pipes/pfc-balance.pipe.ts
@@ -20,67 +20,67 @@ export class PfcBalancePipe implements PipeTransform {
     if (breakfast || lunch || dinner) {
       if (nutrition === 'carbohydrate') {
         breakfast.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodTotalCarbohydrate, 4);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setTotalCarbohydrate, 4);
           }
         });
         lunch.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodTotalCarbohydrate, 4);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setTotalCarbohydrate, 4);
           }
         });
         dinner.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodTotalCarbohydrate, 4);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setTotalCarbohydrate, 4);
           }
         });
       } else if (nutrition === 'protein') {
         breakfast.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodProtein, 4);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setProtein, 4);
           }
         });
         lunch.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodProtein, 4);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setProtein, 4);
           }
         });
         dinner.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodProtein, 4);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setProtein, 4);
           }
         });
       } else if (nutrition === 'fat') {
         breakfast.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodFat, 9);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setFat, 9);
           }
         });
         lunch.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodFat, 9);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setFat, 9);
           }
         });
         dinner.forEach((meal: DailyMeal) => {
-          if (meal.food.foodId) {
+          if (meal.food) {
             calculation(meal.amount, meal.food.foodFat, 9);
-          } else if (meal.set.setId) {
+          } else if (meal.set) {
             calculation(meal.amount, meal.set.setFat, 9);
           }
         });
