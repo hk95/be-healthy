fd04fb2e565238e0f59edeba522216b755790b00
Hideki Kamakura <kakihk695@gmail.com>
2020-09-23 15:52:08 +0900

    feat(envrionment.ts):公開用環境設定

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,21 @@
 export const environment = {
   production: true,
+  stripe: {
+    publicKey:
+      'pk_live_51HFAJnEM1ZTRJUunESWdA5kcp4NNlxqlqgYPi6NxlRw6YzTt0o3Ap650usSRBXLdmB6oj4yYKhz1FVfdDCzD3WFq00O2dUnT24',
+  },
+  algolia: {
+    appId: '2RAZHHXCYL',
+    searchKey: 'f64e94b365e514b3a2266c2f77e7cf7c',
+  },
   firebase: {
-    apiKey: 'AIzaSyANR8AypGILHWh4GjLMk0WkpNEukLpn0-8',
-    authDomain: 'my-app-7e83d.firebaseapp.com',
-    databaseURL: 'https://my-app-7e83d.firebaseio.com',
-    projectId: 'my-app-7e83d',
-    storageBucket: 'my-app-7e83d.appspot.com',
-    messagingSenderId: '512439211847',
-    appId: '1:512439211847:web:e053c145dae6f08a885e62',
-    measurementId: 'G-LTC164CZRL',
+    apiKey: 'AIzaSyC2vENMlNVXJ2AtdyS33LukhwEvIr3TUs8',
+    authDomain: 'be-healthy-prod.firebaseapp.com',
+    databaseURL: 'https://be-healthy-prod.firebaseio.com',
+    projectId: 'be-healthy-prod',
+    storageBucket: 'be-healthy-prod.appspot.com',
+    messagingSenderId: '39559415080',
+    appId: '1:39559415080:web:7dcaa35e99fbedd9193c1d',
+    measurementId: 'G-KXLTTDFXPQ',
   },
 };
