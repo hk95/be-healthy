84fecab345ce923186533cc1225b614d676bdcc0
Hideki Kamakura <kakihk695@gmail.com>
2020-09-24 20:28:12 +0900

    fix(top.component.ts):チュートリアル条件修正#151

--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -69,7 +69,7 @@ export class TopComponent implements OnInit, OnDestroy {
 
   isInitLogin() {
     this.basicInfo$ = this.basicInfoService.getBasicInfo(this.authService.uid);
-    if (!this.authService.isInitialLogin) {
+    if (this.authService.isInitialLogin) {
       this.dialog.open(TutorialComponent, {
         width: '80%',
         maxWidth: '500px',
