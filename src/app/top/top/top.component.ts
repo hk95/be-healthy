bc78701d292d8ce85d9da912c2cebe062f8c543e
Hideki Kamakura <kakihk695@gmail.com>
2020-09-24 20:25:21 +0900

    fix(tutorial.cpmponent):ダイアログ位置内コンテンツ整理#151

--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -69,9 +69,10 @@ export class TopComponent implements OnInit, OnDestroy {
 
   isInitLogin() {
     this.basicInfo$ = this.basicInfoService.getBasicInfo(this.authService.uid);
-    if (this.authService.isInitialLogin) {
+    if (!this.authService.isInitialLogin) {
       this.dialog.open(TutorialComponent, {
-        width: '100%',
+        width: '80%',
+        maxWidth: '500px',
       });
       this.authService.isInitialLogin = false;
     }
