fd002a28beb8823b52ede953ace8373bbd973b44
Hideki Kamakura <kakihk695@gmail.com>
2020-10-01 10:35:55 +0900

    feat(editor-meal.ts):選択中食べもの取得#148

--- src/app/editor-meal/editor-meal/editor-meal.component.ts
+++ src/app/editor-meal/editor-meal/editor-meal.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { DailyMeal } from 'src/app/interfaces/daily-info';
+import { AuthService } from 'src/app/services/auth.service';
+import { DailyInfoService } from 'src/app/services/daily-info.service';
 import { MainShellService } from 'src/app/services/main-shell.service';
 
 @Component({
@@ -9,21 +13,36 @@ import { MainShellService } from 'src/app/services/main-shell.service';
   styleUrls: ['./editor-meal.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class EditorMealComponent implements OnInit {
+export class EditorMealComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
+
+  selectedFoodsOrSets: DailyMeal[];
   date: string;
   meal: string;
 
   constructor(
     private route: ActivatedRoute,
-    private mainShellService: MainShellService
+    private mainShellService: MainShellService,
+    private dailyInfoService: DailyInfoService,
+    private authService: AuthService
   ) {
-    this.route.queryParamMap.subscribe((paramMaps) => {
+    this.subscription = this.route.queryParamMap.subscribe((paramMaps) => {
       this.date = paramMaps.get('date');
       this.meal = paramMaps.get('meal');
       this.mainShellService.setTitle(this.date);
+      this.dailyInfoService
+        .getSelectedFoodsOrSets(this.authService.uid, this.date, this.meal)
+        .subscribe((v) => {
+          this.selectedFoodsOrSets = v;
+          this.mainShellService.setSelectedMeals(this.selectedFoodsOrSets);
+        });
+      this.mainShellService.setTitleMeal(this.meal);
     });
-    this.mainShellService.setTitleMeal(this.meal);
   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
