14b4c045fa5ce6022f61abc3e4aae3586d059fb6
Hideki Kamakura <kakihk695@gmail.com>
2020-10-06 09:56:41 +0900

    feat(meal.guard):add guard for queryParams#152

---
+++ src/app/guards/meal.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MealGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const meal = next.queryParams.meal;
+    if (!(meal === 'breakfast' || meal === 'lunch' || meal === 'dinner')) {
+      return this.router.navigateByUrl('/');
+    } else {
+      return true;
+    }
+  }
+}
