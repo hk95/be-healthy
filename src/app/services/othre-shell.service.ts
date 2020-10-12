64f425dafe8b5022badff7c57d54d9fdd05d892a
Hideki Kamakura <kakihk695@gmail.com>
2020-10-12 21:59:14 +0900

    feat(other-shell.service):タイトル追加#167

--- src/app/services/othre-shell.service.ts
+++ src/app/services/othre-shell.service.ts
@@ -7,6 +7,7 @@ export class OthreShellService {
   readonly PAGE_TITLES = {
     more: 'その他',
     profile: 'ユーザー情報',
+    usage: '使用方法',
     legal: '特定商取引法に基づく表示',
     terms: '利用規約',
     account: 'アカウント削除',
