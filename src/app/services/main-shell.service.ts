d97dfe85c1bf91bb04f0833832579e564010721a
Hideki Kamakura <kakihk695@gmail.com>
2020-10-12 18:56:32 +0900

    feat(main-shell.service):タイトル変数として管理、不要関数削除#167

--- src/app/services/main-shell.service.ts
+++ src/app/services/main-shell.service.ts
@@ -6,25 +6,28 @@ import { DailyMeal } from '../interfaces/daily-info';
   providedIn: 'root',
 })
 export class MainShellService {
-  private readonly titleSource = new ReplaySubject<string>();
-  private readonly titleMealSource = new ReplaySubject<string>();
   private readonly selectedMealsSource = new ReplaySubject<DailyMeal[]>();
   private readonly paymentCompletedSource = new ReplaySubject<void>();
 
-  title$ = this.titleSource.asObservable();
-  titleMeal$ = this.titleMealSource.asObservable();
+  readonly PAGE_TITLES = {
+    top: 'TOP',
+    graph: 'グラフ',
+    menu: 'マイメニュー',
+    more: 'その他',
+    donation: '開発者を支援',
+  };
+  readonly MEAL_TITLES = {
+    breakfast: 'breakfast',
+    lunch: 'lunch',
+    dinner: 'dinner',
+  };
+
+  title: string;
+  titleMeal: string;
   selectedMeals = this.selectedMealsSource.asObservable();
   paymentCompleted$ = this.paymentCompletedSource.asObservable();
 
   constructor() {}
-
-  setTitle(title: string) {
-    this.titleSource.next(title);
-  }
-
-  setTitleMeal(titleMeal: string) {
-    this.titleMealSource.next(titleMeal);
-  }
 
   setSelectedMeals(selectedMeals: DailyMeal[]) {
     this.selectedMealsSource.next(selectedMeals);
