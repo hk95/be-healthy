d68fe10335e8af86fad1f7c76af9b57fa62abeb3
Hideki Kamakura <kakihk695@gmail.com>
2020-10-18 17:06:04 +0900

    feat(menu.html,ts):食事登録ページに戻るボタンの表示切り替え#187

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MainShellService } from 'src/app/services/main-shell.service';
 import { DailyInfoService } from 'src/app/services/daily-info.service';
+import { SetService } from 'src/app/services/set.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
+  readonly mealPageQueryParams = this.dailyInfoService
+    .editorMealPageQueryParams;
   readonly mealCategory = {
     breakfast: '朝食',
     lunch: '昼食',
@@ -15,10 +18,17 @@ export class MenuComponent implements OnInit {
   };
   constructor(
     public dailyInfoService: DailyInfoService,
-    private mainShellService: MainShellService
+    private mainShellService: MainShellService,
+    private setService: SetService
   ) {}
 
   ngOnInit(): void {
     this.mainShellService.title = this.mainShellService.PAGE_TITLES.menu;
+  }
+
+  ngOnDestroy(): void {
+    if (!this.setService.isEditingEditorMeal && this.mealPageQueryParams) {
+      this.dailyInfoService.editorMealPageQueryParams = null;
+    }
   }
 }
