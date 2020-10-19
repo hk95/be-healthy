dff3412ae906cbf307218f70b0c3b28ba1c62a9e
Hideki Kamakura <kakihk695@gmail.com>
2020-10-19 23:07:50 +0900

    fix(auth.service):Observable解除#168

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,24 +4,23 @@ import { auth, User } from 'firebase/app';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  afUser$: Observable<User> = this.afAuth.user;
   uid: string;
+  afUser$: Observable<User> = this.afAuth.user.pipe(
+    tap((user: User) => (this.uid = user?.uid))
+  );
   isInitialLogin: boolean;
 
   constructor(
     private afAuth: AngularFireAuth,
     private router: Router,
     private snackBar: MatSnackBar
-  ) {
-    this.afUser$.subscribe((user) => {
-      this.uid = user?.uid;
-    });
-  }
+  ) {}
 
   login() {
     const provider = new auth.GoogleAuthProvider();
